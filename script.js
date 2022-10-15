b1c8265e5d2b0c36c8fb2c74bebce299040cd900
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2022-10-15 20:08:32 -0300

    javascript finalizado, atende requisitos básicos

--- script.js
+++ script.js
@@ -1,5 +1,71 @@
-function selecionarPrato(prato){
-    prato.classList.toggle("caixa-borda-verde");
+function selecionarPratoPf(prato){
+
+    let sel_anterior = document.querySelector(".caixa-borda-verde-pf");
+    if (sel_anterior !== null){
+        sel_anterior.classList.remove("caixa-borda-verde-pf");
+        let img_sel_anterior = sel_anterior.querySelector(".selecionado-img");
+        img_sel_anterior.classList.remove("ativa-selecionado-img");
+    }
+
+    prato.classList.add("caixa-borda-verde-pf");
     let img_selecionado = prato.querySelector(".selecionado-img");
-    img_selecionado.classList.toggle("ativa-selecionado-img");
+    img_selecionado.classList.add("ativa-selecionado-img");
+
+    if(tresSelecionados()){
+        fecharPedido();
+    }
+}
+
+function selecionarPratoBebida(prato){
+
+    let sel_anterior = document.querySelector(".caixa-borda-verde-bebida");
+    if (sel_anterior !== null){
+        sel_anterior.classList.remove("caixa-borda-verde-bebida");
+        let img_sel_anterior = sel_anterior.querySelector(".selecionado-img");
+        img_sel_anterior.classList.remove("ativa-selecionado-img");
+    }
+
+    prato.classList.add("caixa-borda-verde-bebida");
+    let img_selecionado = prato.querySelector(".selecionado-img");
+    img_selecionado.classList.add("ativa-selecionado-img");
+
+    if(tresSelecionados()){
+        fecharPedido();
+    }
+}
+
+function selecionarPratoSobremesa(prato){
+
+    let sel_anterior = document.querySelector(".caixa-borda-verde-sobremesa");
+    if (sel_anterior !== null){
+        sel_anterior.classList.remove("caixa-borda-verde-sobremesa");
+        let img_sel_anterior = sel_anterior.querySelector(".selecionado-img");
+        img_sel_anterior.classList.remove("ativa-selecionado-img");
+    }
+
+    prato.classList.add("caixa-borda-verde-sobremesa");
+    let img_selecionado = prato.querySelector(".selecionado-img");
+    img_selecionado.classList.add("ativa-selecionado-img");
+
+    if(tresSelecionados()){
+        fecharPedido();
+    }
+}
+
+function tresSelecionados(){
+    let pf = document.querySelector(".caixa-borda-verde-pf");
+    let bebida = document.querySelector(".caixa-borda-verde-bebida");
+    let sobremesa = document.querySelector(".caixa-borda-verde-sobremesa");
+    if ((pf !== null) && (bebida !== null) && (sobremesa !== null)){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function fecharPedido(){
+    let caixa_baixo = document.querySelector(".caixa-texto-baixo");
+    caixa_baixo.classList.add("caixa-texto-baixo-verde");
+    let texto = caixa_baixo.querySelector(".fonte-texto-baixo");
+    texto.innerHTML = "Fechar pedido";
 }
